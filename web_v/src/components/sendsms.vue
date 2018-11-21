55e2fc0fe2efb1804b1e0e5c97358a124c0f2628
qiuyizhuang <qiuyizhuang@baidu.com>
2018-11-21 14:17:39 +0800

    modify for button

---
+++ web_v/src/components/sendsms.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'sendsms',
+  data () {
+    return {
+      msg: 'sendsms'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+      z-index: 2;
+     position:absolute;
+     width: 50px;
+  height:   50px;
+    background: rgba(0, 0, 0, 0);
+  top: 20%;
+  left: 20%;
+  border:none;
+}
+</style>
