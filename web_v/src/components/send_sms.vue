5723bf0b352447d85ecc8d62fafd6ef46c460b89
snow <086634>
2018-11-25 11:17:35 +0800

    添加判断

--- web_v/src/components/send_sms.vue
+++ web_v/src/components/send_sms.vue
@@ -10,7 +10,9 @@
     </el-select>
     
     </el-form-item>
-    <el-form-item label="手机号">
+    <el-form-item label="手机号" required prop="收信人手机号" :rules="[
+    {required:ture,trigger:'blur'}
+    ]">
     <el-input v-model="formInline.remobile" placeholder="收件人手机号" disable="isable" clearable></el-input>
   </el-form-item>
    <el-form-item label="手机号">
@@ -25,7 +27,9 @@
 
 <div class="input">送信内容（表达歉意、告白、祝福等，推荐与对方面对面进行；若联系不上“比如被拉黑”，可采用此方式。切不可频繁发送打扰对方的生活）
    
-  <el-input label="内容" type="textarea" :rows="8" id="area" maxlength="1340" v-model="textarea" placeholder="请输入内容，不允许特殊字符，不超过1340个字"  clearable></el-input>
+  <el-input label="内容" type="textarea" :rows="8" id="area" maxlength="1340" v-model="textarea" placeholder="请输入内容，不允许特殊字符，不超过1340个字 
+禁止发送股票、移民、面试招聘、彩票、返利、贷款、催款、投资理财、赌博、中奖、一元夺宝、一元秒杀、A货、医疗、整形、美容、会所、酒吧、足浴、暴力、恐吓、色情、皮草、助考，商标注册、加群、加QQ或者加微信、贩卖个人信息、宣传短信通道、游戏推广、会展推广、网站推广、优惠券类推广、卡类推广、保险推广、酒类、用户拉新、用户召回，另外金融、房地产、教育这几类" 
+   clearable></el-input>
   <p><span id="text-count">字数限制</span>1340个</p>
 
   </div>
@@ -40,7 +44,7 @@ todo:
 2、输入框剩余字数统计、特殊字符校验、内容加缓存
 3、数据封装、传后端接口    done
 4、选择说吧时，弹出提示框，功能开发中 done
-5、查询按钮放置、帮助页面
+5、查询按钮放置、帮助页面  
 6、说吧页面显示，弹框提示  done
  */
   export default {
@@ -77,7 +81,28 @@ todo:
         var remobile=this.formInline.remobile;
         var sendmobile=this.formInline.sendmobile;
         var textarea=this.textarea;
-        
+        /* 参数判断 */
+         if(region=="说吧"){
+            
+            this.$alert("说吧功能开发中", '提示', {
+          confirmButtonText: '确定',
+          callback: action => {
+               this.formInline.region=="";
+          },
+          
+        });
+          
+          }
+          else if (region=="") {
+            this.$alert("类型不能为空", '提示', {
+          confirmButtonText: '确定',        
+        });
+          }
+          else if (remobile=="") {
+            this.$alert("收信人手机号不能为空", '提示', {
+          confirmButtonText: '确定',        
+        });
+          }
         var postdata = {
           textarea:textarea,
           remobile:remobile,
@@ -142,7 +167,7 @@ todo:
 .hello{
       z-index: 2;
      position:absolute;
-    width: 920px;
+    width: 930px;
   height:   200px;
     background: rgba(0, 0, 0, 0);
   top: 30%;
@@ -153,7 +178,7 @@ todo:
 .label_send{
       z-index: 2;
      position:absolute;
-    width: 920px;
+    width: 930px;
   height:   100px;
     background: rgba(0, 0, 0, 0);
 
@@ -162,7 +187,7 @@ todo:
 .input{
   z-index: 2;
   position:absolute;
-  width: 920px;
+  width: 930px;
   height:   100px;
   top: 40%;
   font-size: large;
