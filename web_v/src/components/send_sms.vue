bbf67eb6f70b379b053ec42c4ae0dacd2fe44dcd
qiuyizhuang <qiuyizhuang@baidu.com>
2018-11-26 18:46:04 +0800

    add text info

--- web_v/src/components/send_sms.vue
+++ web_v/src/components/send_sms.vue
@@ -10,13 +10,17 @@
     </el-select>
     
     </el-form-item>
-    <el-form-item label="手机号" required prop="收信人手机号" :rules="[
-    {required:ture,trigger:'blur'}
+    <el-form-item label="手机号" required  prop="remobile" :rules="[
+    {required:ture, message:'必须为11位可用手机号',trigger:'blur'}
+    
     ]">
-    <el-input v-model="formInline.remobile" placeholder="收件人手机号" disable="isable" clearable></el-input>
+    <el-input v-model="formInline.remobile" id="remob"  placeholder="收件人手机号" disable="isable" @keyup.native="checknum" clearable></el-input>
   </el-form-item>
-   <el-form-item label="手机号">
-    <el-input v-model="formInline.sendmobile" placeholder="本人手机号，用于接收回复" disable="isable" clearable></el-input>
+   <el-form-item label="手机号"  prop="sendmobile"  :rules="[
+    {required:ture, message:'必须为11位可用手机号',trigger:'blur'}
+    
+    ]">
+    <el-input v-model="formInline.sendmobile"   placeholder="本人手机号，用于接收回复" disable="isable" clearable></el-input>
   </el-form-item>
     <el-form-item>
     <el-button type="primary" @click="onSubmit()">送信</el-button>
@@ -27,11 +31,11 @@
 
 <div class="input">送信内容（表达歉意、告白、祝福等，推荐与对方面对面进行；若联系不上“比如被拉黑”，可采用此方式。切不可频繁发送打扰对方的生活）
    
-  <el-input label="内容" type="textarea" :rows="8" id="area" maxlength="1340" v-model="textarea" placeholder="请输入内容，不允许特殊字符，不超过1340个字 
+  <el-input label="内容" type="textarea" :rows="8" id="area" maxlength="1340" @input="word" v-model="textarea" placeholder="请输入内容，不允许特殊字符，不超过1340个字 
 禁止发送股票、移民、面试招聘、彩票、返利、贷款、催款、投资理财、赌博、中奖、一元夺宝、一元秒杀、A货、医疗、整形、美容、会所、酒吧、足浴、暴力、恐吓、色情、皮草、助考，商标注册、加群、加QQ或者加微信、贩卖个人信息、宣传短信通道、游戏推广、会展推广、网站推广、优惠券类推广、卡类推广、保险推广、酒类、用户拉新、用户召回，另外金融、房地产、教育这几类
-" 
+问题交流反馈加QQ群：482818592" 
    clearable></el-input>
-  <p><span id="text-count">字数限制</span>1340个</p>
+  <p><span id="text-count">字数限制</span>{{word_num}}/1340个</p>
 
   </div>
 </div>
@@ -42,7 +46,7 @@
 /*
 todo:
 1、手机号格式校验
-2、输入框剩余字数统计、特殊字符校验、内容加缓存
+2、输入框剩余字数统计(done)、特殊字符校验、内容加缓存
 3、数据封装、传后端接口    done
 4、选择说吧时，弹出提示框，功能开发中 done
 5、查询按钮放置、帮助页面  
@@ -53,18 +57,35 @@ todo:
     
     name: 'send_sms',
     data() {
+      
+        
+        
+      
       return {
+        word_num: 1340,
         textarea: '',
         isable:'false',
         formInline: {
           remobile: '',
           sendmobile: '',
           region: ''
-        }
+        },
+       
+        
       }
     },
     methods: {
-      change: function(value) {
+       word: function(){
+         var txt=this.textarea.length;
+         this.word_num=1340-txt;
+       },
+     
+       checknum: function(){
+        this.formInline.remobile=this.formInline.remobile.replace(/[^\.\d]/g,'');
+        this.formInline.remobile=this.formInline.remobile.replace('.','');
+        
+         },
+      change: function() {
           if(this.formInline.region=="说吧"){
             
             this.$alert("说吧功能开发中", '提示', {
